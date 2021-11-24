befbd79272ba9e84c96afbe2835d11c77b16280b
Yulia-kom <85898864+Yulia-kom@users.noreply.github.com>
2021-11-24 21:28:36 +0200

    start

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,10 @@
+const bntStart = document.querySelector("data-start");
+const bntStop = document.querySelector("data-stop");
+
+
+
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
