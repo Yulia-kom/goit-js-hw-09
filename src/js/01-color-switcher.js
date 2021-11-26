9d8c7e5dad87e1142a92b9770ef6aba3c9d7791f
Yulia-kom <85898864+Yulia-kom@users.noreply.github.com>
2021-11-26 22:12:26 +0200

    01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,8 +1,23 @@
-const bntStart = document.querySelector("data-start");
-const bntStop = document.querySelector("data-stop");
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+const body = document.querySelector("body");
+
+let timerId = null;
 
 
+startBtn.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        const color = getRandomHexColor();
+        body.style.backgroundColor = color;
+    }, 1000);
+    startBtn.disabled = true;
 
+});
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+});
 
 
 function getRandomHexColor() {
